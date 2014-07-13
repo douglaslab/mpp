ae4869f2b83422d39caca73638130721da0a46fa
sdouglas <shawn.douglas@gmail.com>
2014-07-13 15:46:31 -0700

    convert pubmed xml → static markdown files

---
+++ code/json_to_markdown.py
@@ -0,0 +1,110 @@
+#!/usr/bin/env python
+# encoding: utf-8
+"""
+json_to_markdown.py
+
+Created on 2014-07-13.
+
+The MIT License (MIT)
+
+Copyright (c) 2014. Shawn Douglas
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+"""
+
+import sys
+import os
+import json
+
+filename = "../data/json/2014-07-13_pubmed_result.json"
+
+
+post_template = """
+---
+layout: post
+title:  "%s"
+date:   %s 12:00:00
+category:  publication
+published: true
+pmid: %s
+---
+
+Title: %s
+
+Authors: %s
+
+Journal: %s
+
+Abstract: %s
+
+PMID: %s
+
+"""
+
+# 
+
+with open(filename) as json_data:
+    d = json.load(json_data)
+    articles = d["PubmedArticleSet"]["PubmedArticle"]
+    for article in articles:
+        citation = article["MedlineCitation"]
+        pmid = citation["PMID"]["#text"]
+        print pmid
+        date = citation["DateCreated"]
+        dateStr = "%s-%s-%s" % (date["Year"], date["Month"], date["Day"])
+
+        journal = citation["Article"]["Journal"]
+        journalname = journal["ISOAbbreviation"]
+        pubdate = journal["JournalIssue"]["PubDate"]
+        
+        if "Month" in pubdate and "Day" in pubdate:
+            pubdateStr = "%s %s %s" % (pubdate["Year"], pubdate["Month"], pubdate["Day"])
+        else:
+            pubdateStr = pubdate["Year"]
+
+        ji = journal["JournalIssue"]
+        vipStr = ""
+        if "Volume" in ji and "Issue" in ji:
+            vipStr = "**%s**(%s)" % (ji["Volume"], ji["Issue"])
+            if "Pagination" in citation["Article"]:
+                vipStr += ":%s" % citation["Article"]["Pagination"]["MedlinePgn"]
+            citeStr = "*%s %s;%s*" % (journalname, pubdateStr, vipStr)
+        else:
+            citeStr = "*%s %s*" % (journalname, pubdateStr)
+
+        # print citeStr
+        title = citation["Article"]["ArticleTitle"]
+        abstract = citation["Article"]["Abstract"]["AbstractText"]
+        authorCount = len(citation["Article"]["AuthorList"]["Author"])
+        authors = citation["Article"]["AuthorList"]["Author"]
+        if type(authors) is dict:
+            authors = [authors]
+        authorList = []
+        for author in authors:
+            authorList.append("%s %s" % (author["LastName"], author["Initials"]))
+        authorStr = ", ".join(authorList)
+        firstAuthorLN = authors[0]["LastName"].lower()
+        mdFilename = "%s-%s.md" % (dateStr, firstAuthorLN)
+        mdContent = post_template % (title, dateStr, pmid, title, authorStr, citeStr, abstract, pmid)
+        mdContent = mdContent.encode("utf-8")
+        print mdFilename
+        print mdContent
+        f = open("../data/markdown/"+mdFilename, 'w')
+        f.write(mdContent)
+        f.close()
---
+++ data/markdown/2004-12-16-rothemund.md
@@ -0,0 +1,20 @@
+
+---
+layout: post
+title:  "Design and characterization of programmable DNA nanotubes."
+date:   2004-12-16 12:00:00
+category:  publication
+published: true
+pmid: 15600335
+---
+
+Title: Design and characterization of programmable DNA nanotubes.
+
+Authors: Rothemund PW, Ekani-Nkodo A, Papadakis N, Kumar A, Fygenson DK, Winfree E
+
+Journal: *J. Am. Chem. Soc. 2004 Dec 22;**126**(50):16344-52*
+
+Abstract: DNA self-assembly provides a programmable bottom-up approach for the synthesis of complex structures from nanoscale components. Although nanotubes are a fundamental form encountered in tile-based DNA self-assembly, the factors governing tube structure remain poorly understood. Here we report and characterize a new type of nanotube made from DNA double-crossover molecules (DAE-E tiles). Unmodified tubes range from 7 to 20 nm in diameter (4 to 10 tiles in circumference), grow as long as 50 microm with a persistence length of approximately 4 microm, and can be programmed to display a variety of patterns. A survey of modifications (1) confirms the importance of sticky-end stacking, (2) confirms the identity of the inside and outside faces of the tubes, and (3) identifies features of the tiles that profoundly affect the size and morphology of the tubes. Supported by these results, nanotube structure is explained by a simple model based on the geometry and energetics of B-form DNA.
+
+PMID: 15600335
+
